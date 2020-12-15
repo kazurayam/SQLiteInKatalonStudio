0438d89a9b0249c55734bca887437ec36715bd0a
kazurayam <kazuaki.urayama@gmail.com>
2020-12-15 12:21:45 +0900

    fixed a bug in SQLiteDataSetExample

--- Keywords/my/SQLiteDataSetExample.groovy
+++ Keywords/my/SQLiteDataSetExample.groovy
@@ -23,16 +23,16 @@ import groovy.sql.Sql
 import groovy.sql.DataSet
 
 public class SQLiteDataSetExample {
-	
+
 	static void run() {
-		
+
 		/**
 		 * https://blog.mrhaki.com/2009/10/groovy-goodness-groovy-sql-dataset.html
 		 *
 		 */
 		// Create connection to SQLite with classic JDBC DriverManager.
 		Sql db = Sql.newInstance("jdbc:sqlite:sample2.db", "org.sqlite.JDBC")
-		 
+
 		// Create a new table
 		db.execute 'drop table if exists languages'
 		db.execute '''
@@ -45,35 +45,37 @@ public class SQLiteDataSetExample {
 		//When you create a table without specifying the WITHOUT ROWID option,
 		//SQLite adds an implicit column called rowid that stores 64-bit signed integer.
 		//The rowid column is a key that uniquely identifies the rows in the table.
-		 
+
 		// Create a DataSet.
 		DataSet languageSet = db.dataSet("languages")
 		languageSet.add(name: 'Groovy')
 		languageSet.add(name: 'Java')
 		languageSet.add(name: 'JRuby')
 		languageSet.add(name: 'Scala')
-		
+
 		// Get data with each method.
 		List<String> result = []
-		languageSet.each {
-			result << it.name
-		}
+		languageSet.each { result << it.name }
 		assert 4 == result.size()
-		assert ['Groovy', 'Java', 'JRuby', 'Scala'] == result
-		 
+		assert [
+			'Groovy',
+			'Java',
+			'JRuby',
+			'Scala'] == result
+
 		// Use findAll and sort to define a query condition.
 		DataSet firstItems = languageSet.findAll{ it.rowid < 3 }.sort{ it.name }
 		// No database acccess yet, only the query is constructed.
 		// We can see the query with getSql()
 		assert 'select * from languages where rowid < ? order by name' == firstItems.getSql()
 		// We can see the parameters with getParameters()
-		assert [3] == firstItems.getParameters()
-		 
+		assert [3]== firstItems.getParameters()
+
 		// We call each to really access the database.
 		firstItems.findAll { it.name == 'Groovy' }.each { row ->
 			//assert 1 == row.rowid
 			assert 'Groovy' == row.name
 		}
-		
+
 	}
 }
